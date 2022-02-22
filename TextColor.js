8649f15306f4cd77438a8dcab89d82c2acefe2bf
RoutalMounir <routalmounir@gmail.com>
2022-02-22 01:38:41 +0100

    first commit

---
+++ TextColor.js
@@ -0,0 +1,8 @@
+var text = "Color";
+var i;
+for (i = 0; i < text.length; i++) {
+	document.writeln(text.charCodeAt(i));
+	//document.writeln("<br><h1 style=\"color:" + i * 100 + "f\">" + text.charAt(i) + "</h1>");
+	//document.writeln("<br><h1 style=\"background-color:rgb(255, 11, 71);" + text.charAt(i) + "</h1>");
+	
+}
